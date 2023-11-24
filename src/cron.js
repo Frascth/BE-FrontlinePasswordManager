a2fea0083ff76f23f03c70af30c6066d2d55ae80
Aptanta Jodi M <gizrouth@gmail.com>
2023-11-24 17:55:15 +0700

    add reset authState cron

--- src/cron.js
+++ src/cron.js
@@ -4,6 +4,32 @@ import { scheduleJob } from 'node-schedule';
 import T3User from './models/T3User.js';
 import { USER_AUTH_STATE } from './utils/constant.js';
 
+async function resetAuthState() {
+  // run once every server start / restart
+  const updatePromise = [];
+  const users = await T3User.findAll({
+    where: {
+      isDeleted: false,
+      authState: {
+        [Op.ne]: USER_AUTH_STATE.LOGOUT,
+      },
+    },
+  });
+
+  if (users) {
+    users.forEach((user) => {
+      updatePromise.push(async () => {
+        user.authState = USER_AUTH_STATE.LOGOUT;
+        await user.save();
+      });
+    });
+  }
+
+  await Promise.all(updatePromise.map((asyncFunc) => asyncFunc()));
+  console.log('resetAuthState once every server start/restart done');
+
+}
+
 async function resetSession() {
   // every 1 minutes
   // check if user already in otp or in login session for 30 or more minutes, then logout
@@ -33,6 +59,7 @@ async function resetSession() {
 }
 
 function initCron() {
+  resetAuthState();
   scheduleJob('0/1 * * * *', resetSession);
 }
 
