bcc95cc3d4dc9b340a18250ec27e40403a33d500
Aptanta Jodi M <gizrouth@gmail.com>
2023-12-12 12:33:57 +0700

    restructure avoid circular import

--- src/models/T3Otp.js
+++ src/models/T3Otp.js
@@ -3,26 +3,33 @@ import { DataTypes, Model } from 'sequelize';
 import { sequelizeConn } from '../dbConnection.js';
 import Util from '../utils/Util.js';
 import T1MessageContent from './T1MessageContent.js';
-import { HTML_CONTENT, MESSAGE_CONTENT } from '../utils/constant.js';
+import { HTML_CONTENT, MESSAGE_CONTENT, TWO_FAC_AUTH } from '../utils/constant.js';
 import T1HtmlContent from './T1HtmlContent.js';
-import T3User from './T3User.js';
 
 class T3Otp extends Model {
 
-  static async sendOtpWa(to, userFk) {
+  static async sendOtp(userModel) {
+    if (userModel.twoFacAuth === TWO_FAC_AUTH.TOTP_WA) {
+      T3Otp.sendOtpWa(userModel);
+    } else if (userModel.twoFacAuth === TWO_FAC_AUTH.TOTP_GMAIL) {
+      T3Otp.sendOtpEmail(userModel);
+    }
+  }
+
+  static async sendOtpWa(userModel) {
     const transaction = await sequelizeConn.transaction();
 
     try {
       const otpCode = Util.generateRandomNumber(6);
       const { hashedText: hashedOtpCode } = await Util.hashText(otpCode);
       await this.create({
-        userFk,
+        userFk: userModel.pk,
         otpCode: hashedOtpCode,
       }, { transaction });
       // eslint-disable-next-line max-len
       let { content: message } = await T1MessageContent.findOne({ where: { pk: MESSAGE_CONTENT.TOTP_LOGIN } });
       message = message.replace('{{otpCode}}', otpCode);
-      await Util.sendWhatsApp(to, message);
+      Util.sendWhatsApp(userModel.waNumber, message);
       await transaction.commit();
     } catch (error) {
       await transaction.rollback();
@@ -31,23 +38,22 @@ class T3Otp extends Model {
 
   }
 
-  static async sendOtpEmail(to, userFk = 'anonym') {
+  static async sendOtpEmail(userModel) {
     const transaction = await sequelizeConn.transaction();
 
     try {
-      const { username } = await T3User.findOne({ where: { pk: userFk } });
       const otpCode = Util.generateRandomNumber(6);
       const { hashedText: hashedOtpCode } = await Util.hashText(otpCode);
       await T3Otp.create({
-        userFk,
+        userFk: userModel.pk,
         otpCode: hashedOtpCode,
       }, { transaction });
 
       // eslint-disable-next-line max-len
       let { subject, content: message } = await T1HtmlContent.findOne({ where: { pk: HTML_CONTENT.TOTP_LOGIN } });
       message = message.replace('{{otpCode}}', otpCode);
-      message = message.replace('{{username}}', username);
-      await Util.sendMail(to, subject, message);
+      message = message.replace('{{username}}', userModel.username);
+      Util.sendMail(userModel.email, subject, message);
     } catch (error) {
       await transaction.rollback();
       throw error;
